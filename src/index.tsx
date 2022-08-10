eab55039ea0b474c7f8fc5bd748c03a16a70b0a3
Julian Rivera <julianarevircoins@gmail.com>
2022-08-10 16:49:02 -0400

    jbook

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import * as esbuild from 'esbuild-wasm';
+import ReactDOM from "react-dom"; 
+import {useState, useEffect, useRef} from "react"  
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const App =() =>{
+    const [input, setInput] = useState('')
+    const [code, setCode] = useState('')
+
+    const ref = useRef<any>();
+
+    const startService = async() =>{
+        // this we use to transpile our code.
+        // we can use this ref anywhere in our component now
+        ref.current = await esbuild.startService({
+            worker:true,
+            wasmURL: '/esbuild.wasm'
+        })
+        
+    }
+
+    useEffect(()=>{
+        startService()
+    }, [])
+    const onClick = async () =>{
+        // stops the error message if users immediately click the submit button without putting anything inside
+        if(!ref.current)
+        return;
+        
+      const result = await ref.current.transform(input, {
+        loader: 'jsx',
+        target:'es2015'
+       });
+
+      setCode(result.code);
+    };
+    return <div>
+        <textarea value={input} onChange={e=>{
+            setInput(e.target.value)
+        }}></textarea>
+        <div>
+            <button onClick={onClick}>Submit</button>
+        </div>
+        <pre>{code}</pre>
+    </div>
+}
+ReactDOM.render(<App />, document.querySelector('#root'))
