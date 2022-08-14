5cb5f0f5aacd9a945e98198987b43e89146c7528
Julian Rivera <julianarevircoins@gmail.com>
2022-08-13 21:40:36 -0400

    mediumtestpkg/new url included

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,4 +1,5 @@
 import * as esbuild from 'esbuild-wasm';
+import axios from 'axios'
  
 export const unpkgPathPlugin = () => {
   return {
@@ -8,7 +9,23 @@ export const unpkgPathPlugin = () => {
     setup(build: esbuild.PluginBuild) {
       build.onResolve({ filter: /.*/ }, async (args: any) => {
         console.log('onResole', args);
+        if(args.path === 'index.js'){
         return { path: args.path, namespace: 'a' };
+        }
+        // } else if (args.path === 'tiny-test-pkg'){
+        //     return {path: 'https://unpkg.com/tiny-test-pkg@1.0.0/index.js',
+        // namespace: 'a'}
+        // }
+        if(args.path.includes('./') || args.path.includes('../')){
+            return {
+                namespace:'a',
+                path: new URL(args.path, args.importer + '/').href
+            };
+        
+        }
+        return {
+            namespace:'a', path:`https://unpkg.com/${args.path}`
+        }
       });
 //  overrides esbuild way of loading up a file, which is to read it off a filesystem
 // so this says dont worry about it, we'll return this object
@@ -19,11 +36,16 @@ export const unpkgPathPlugin = () => {
           return {
             loader: 'jsx',
             contents: `
-              import message from 'tiny-test-pkg';
+              const message = require('medium-test-pkg');
               console.log(message);
             `,
           };
         } 
+        const  {data} = await axios.get(args.path)
+        return {
+            loader: 'jsx',
+            contents: data
+        }
       });
     },
   };
