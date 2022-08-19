585996575f0fe56e45870a3756535baed79ecc90
Julian Rivera <julianarevircoins@gmail.com>
2022-08-19 01:03:13 -0400

    fixing iframe/using srcdocs/section 10 complete

--- src/index.tsx
+++ src/index.tsx
@@ -7,9 +7,10 @@ import { fetchPlugin } from './plugins/fetch-plugin';
 
 const App =() =>{
     const [input, setInput] = useState('')
-    const [code, setCode] = useState('')
+  
 
     const ref = useRef<any>();
+    const iframe = useRef<any>();
 
     const startService = async() =>{
         // this we use to transpile our code.
@@ -26,9 +27,11 @@ const App =() =>{
     }, [])
     const onClick = async () =>{
         // stops the error message if users immediately click the submit button without putting anything inside
-        if(!ref.current)
+        if(!ref.current){
         return;
-        
+        }
+        // resets the content of iframe
+        iframe.current.srcdoc = html;
     //   const result = await ref.current.transform(input, {
     //     loader: 'jsx',
     //     target:'es2015'
@@ -46,16 +49,35 @@ const App =() =>{
         })
         console.log(result)
         // contains out transpiled and bundle code here.
-      setCode(result.outputFiles[0].text);
-
+    //   setCode(result.outputFiles[0].text);
+iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*')
     //   built in to browser function
-    try{
-    eval(result.outputFiles[0].text);
-    }
-    catch(err){
-        alert(err);
-    }
+  
     };
+
+    const html = `<html>
+    <head></head>
+    <body>
+    <div id="root"></div>
+    <script>
+
+    window.addEventListener('message', (event) =>{
+        try{
+eval(event.data)
+        }
+        catch(err){
+const root = document.querySelector('#root')
+root.innerHTML = '<div style="color:red;"><h4>Runtime Error</h4>' + err + '</div>'
+
+throw err;
+        }
+    },false)
+    </script>
+    </body>
+    
+    </html>`
+
+    // if we put throw err below the error on box then it will console.log our error as well
     return <div>
         <textarea value={input} onChange={e=>{
             setInput(e.target.value)
@@ -63,9 +85,11 @@ const App =() =>{
         <div>
             <button onClick={onClick}>Submit</button>
         </div>
-        <pre>{code}</pre>
+     
         {/* embed a document into another document html */}
-        <iframe sandbox="" src="/test.html" />
+        {/* srcdoc html makes request to local string instead of some outside url, we also cant use localstorage/cookies like thid */}
+        <iframe title="code preview" ref={iframe} sandbox="allow-scripts" srcDoc={html}/>
     </div>
 }
+
 ReactDOM.render(<App />, document.querySelector('#root'))
