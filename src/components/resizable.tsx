4a6547fc8c862768afeb1d56e68a8ba55c6c76f1
Julian Rivera <julianarevircoins@gmail.com>
2022-08-22 01:13:35 -0400

    width fixing

--- src/components/resizable.tsx
+++ src/components/resizable.tsx
@@ -1,5 +1,6 @@
 import { ResizableBox, ResizableBoxProps } from "react-resizable";
 import './resizable.css';
+import {useEffect, useState} from 'react';
 
 interface ResizableProps {
     // if we want to allow only certain strings, we can write them out like this.
@@ -10,14 +11,45 @@ interface ResizableProps {
 const Resizable: React.FC<ResizableProps> = ({direction, children}) => {
 
     let resizeableProps: ResizableBoxProps;
+    const [innerHeight, setInnerHeight] = useState(window.innerHeight);
+    const [innerWidth, setInnerWidth] = useState(window.innerWidth);
+    const [width, setWidth] = useState(window.innerWidth * 0.75);
+    useEffect(()=>{
+        let timer: any;
+const listener = () =>{
+if(timer) {
+    clearTimeout(timer);
+}
+   timer = setTimeout(()=>{
+        setInnerHeight(window.innerHeight);
+        setInnerWidth(window.innerWidth);
+        if(window.innerWidth * 0.75 < width){
+            setWidth(window.innerWidth * 0.75)
+        }
+    },100)
+   
+
+
+}
+window.addEventListener('resize', listener)
+
+return () =>{
+    window.removeEventListener('resize', listener)
+}
+    }, [])
     if(direction === 'horizontal'){
         resizeableProps = {
-            minConstraints:[window.innerWidth * 0.2, Infinity], maxConstraints:[window.innerWidth * 0.75, Infinity],height:Infinity, width:window.innerWidth * 0.75, resizeHandles:['e']
+            className: 'resize-horizontal',
+            minConstraints:[innerWidth * 0.2, Infinity], maxConstraints:[innerWidth * 0.75, Infinity],height:Infinity, width, resizeHandles:['e'],
+            onResizeStop: (event, data) =>{
+                setWidth(data.size.width);
+
+            }
         };
     }
     else{
         resizeableProps ={
-            minConstraints:[Infinity, 24], maxConstraints:[Infinity, window.innerHeight * 0.9],height:300, width:Infinity, resizeHandles:['s']
+            minConstraints:[Infinity, 24], maxConstraints:[Infinity, innerHeight * 0.9],height:300, width:Infinity, resizeHandles:['s']
 
         }
     }
