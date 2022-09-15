500886cece941d81eaf2144c10aad998277b6bc3
Julian Rivera <julianarevircoins@gmail.com>
2022-09-14 23:58:53 -0400

    implemented action bar

--- src/components/text-editor.tsx
+++ src/components/text-editor.tsx
@@ -1,12 +1,19 @@
 import MDEditor from "@uiw/react-md-editor";
 import {useState, useEffect, useRef} from 'react'
 import './text-editor.css';
+import {Cell} from '../state'
+import {useActions} from '../hooks/use-actions'
 
+
+interface TextEditorProps {
+    cell:Cell
+}
 // type of component react function component
-const TextEditor: React.FC = ()=>{
+const TextEditor: React.FC<TextEditorProps> = ({cell})=>{
     const ref = useRef<HTMLDivElement | null> (null);
 const [editing, setEditing] = useState(false)
-const [value,setValue] = useState('# Header')
+
+const {updateCell} = useActions();
 
 useEffect(()=>{
     const listener = (event: MouseEvent) =>{
@@ -28,8 +35,8 @@ setEditing(false)
 if(editing){
     return (
         <div  className="text-editor" ref={ref}>
-            <MDEditor value={value} onChange={(v)=>{
-setValue(v || '')
+            <MDEditor value={cell.content} onChange={(v)=>{
+updateCell(cell.id, v || '')
             }}/>
         </div>
     )
@@ -38,7 +45,7 @@ return <div className="text-editor card" onClick={()=>{
     setEditing(true)
 }}>
     <div className="card-content">
-<MDEditor.Markdown source={value}/>
+<MDEditor.Markdown source={cell.content || 'Click to edit'}/>
 </div>
     </div>
 }
