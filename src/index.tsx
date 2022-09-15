98ee15aafee69f4b9c685bc8b3e89fedbbaf0b7b
Julian Rivera <julianarevircoins@gmail.com>
2022-09-15 00:23:12 -0400

    implemented icon buttons

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import ReactDOM from "react-dom";
 import {Provider} from 'react-redux';
 import {store} from './state'
 import  'bulmaswatch/superhero/bulmaswatch.min.css'
+import '@fortawesome/fontawesome-free/css/all.min.css'
 // import CodeCell from "./components/code-cell";
 // import TextEditor from "./components/text-editor";
 import CellList from "./components/cell-list";
